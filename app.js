13a24754b691138f3f3600e76b3a8bda87158065
omarbr44 <omarbr44@users.noreply.github.com>
2025-03-20 04:03:05 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// tailwind configuration
+tailwind.config = {
+    theme: {
+        extend: {
+            colors:{
+            "site-primary": "#fe4c25",
+            "site-secondary": "#0d0e13",
+            "site-section": "#f5f5f5",
+            "site-p": "#364152",
+            }
+        },
+        fontFamily:{
+            Roboto: ["Roboto, sans-serif"],
+        },
+        container: {
+            padding: "0.5rem",
+            center: true,
+        },
+},
+}
